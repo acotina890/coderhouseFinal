5f8e52a755b6d8d115c8b2fc9b67493c557ed1a1
TCT <102008487+TCT1@users.noreply.github.com>
2022-07-20 18:10:53 -0500

    Movimiento V0.0.2
    Se agregó la dinámica de curación y la habilidad de correr

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public bool Healed = false;
     public bool canWalk;
     public Animator anim;
     public Rigidbody rb;
     Vector3 rotationtnput = Vector3.zero;
     public float rotSens = 10;
-    public float speed;
+    public float speed = 4;
 
     void Start(){
         canWalk = false;
     }
 
-    void Update()
+    void FixedUpdate()
     {
         if(canWalk){
             Look();
             Move();
+            InteractHealing();
         }
     }
 
@@ -36,6 +38,16 @@ public class PlayerMovement : MonoBehaviour
             Vector3 direction = (transform.forward * ver + transform.right * hor).normalized;
             rb.velocity = direction * speed;
             anim.SetBool("Walking", true);
+            if(Healed){
+                if(Input.GetKey(KeyCode.LeftShift)){
+                    anim.SetBool("Running", true);
+                    speed = 12;
+                }
+                else{
+                    anim.SetBool("Running", false);
+                    speed = 6;
+                }
+            }
         }
         else{
             rb.velocity = Vector3.zero;
@@ -43,7 +55,20 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    private void InteractHealing(){
+        if(Input.GetKey("t")){
+            Healed = true;
+            anim.SetBool("Walking", false);
+            anim.SetTrigger("PickUpHealing");
+            speed = 6;
+        }
+    }
+
     public void CanWalk(){
         canWalk = true;
+    }
+
+    public void NoWalk(){
+        canWalk = false;
     }
 }
